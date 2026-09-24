ea9d869acbd9fe1f51f3671b0bd8a5c37f822c60
bolachariaelainemara <bolachariaelainemara@gmail.com>
2026-09-24 11:56:19 -0300

    Create analytics.js

---
+++ analytics.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-2CBFDQ8B53"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-JQTR766PBR');
+</script>
