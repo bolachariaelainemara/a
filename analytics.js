b6c06f76798977492e3ce412143afc70a683754d
bolachariaelainemara <bolachariaelainemara@gmail.com>
2026-09-24 12:24:19 -0300

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -1,3 +1,4 @@
+
 <!-- 1º LUGAR: No link que descarrega a biblioteca do Google -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-JQTR766PBR"></script>
 
@@ -6,6 +7,6 @@
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  <!-- 2º LUGAR: Na linha de instrução para ativar a sua conta -->
+  // 2º LUGAR: Na linha de instrução para ativar a sua conta
   gtag('config', 'G-JQTR766PBR');
 </script>
